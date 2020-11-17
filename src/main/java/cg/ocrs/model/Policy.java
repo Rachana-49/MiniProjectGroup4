a9daa697258a117c66a99f825180765cbeb7e81f
G.V.Ramana <G.V.Ramana@pallavi>
2020-11-17 15:21:59 +0530

    commit and push

---
+++ src/main/java/cg/ocrs/model/Policy.java
@@ -0,0 +1,26 @@
+package cg.ocrs.model;
+
+public class Policy {
+private int policyNumber;
+private float policyPremium;
+private int accountNumber;
+public int getPolicyNumber() {
+	return policyNumber;
+}
+public void setPolicyNumber(int policyNumber) {
+	this.policyNumber = policyNumber;
+}
+public float getPolicyPremium() {
+	return policyPremium;
+}
+public void setPolicyPremium(float policyPremium) {
+	this.policyPremium = policyPremium;
+}
+public int getAccountNumber() {
+	return accountNumber;
+}
+public void setAccountNumber(int accountNumber) {
+	this.accountNumber = accountNumber;
+}
+
+}
